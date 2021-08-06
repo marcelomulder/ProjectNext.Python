9ca4344bb596574cc39bd6d48cdb4b25c64a8c7e
Marcelo Queiroz <84861244+marcelomulder@users.noreply.github.com>
2021-08-06 16:34:35 -0300

    Add files via upload

--- exercicio03aula4.py
+++ exercicio03aula4.py
@@ -1,10 +1,12 @@
 # 3 . Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
-primos = [2]
+primos = []
 verificador =  False
 num = int(input("Insira um número inteiro maior que 1: "))
 if num <= 1:
     print("Número inválido.")
 else:
+    if num > 1:
+        primos.append(2)
     for i in range(2,num+1):
         j = 2
         while (i % j != 0 and i >= j):
